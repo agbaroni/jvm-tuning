73a5ef2cf6fb9dcbcafd39e7e9d26c3beff90155
Alessio G. Baroni <alessiogiovanni.baroni@gmail.com>
2021-02-27 11:35:44 +0100

    Created 'empty' project

---
+++ empty/src/main/java/com/redhat/abaroni/jvm/tuning/empty/Main.java
@@ -0,0 +1,7 @@
+package com.redhat.abaroni.jvm.tuning.empty;
+
+public class Main {
+    public static void main(String... args) {
+	while (true);
+    }
+}
---
+++ empty/src/main/java/com/redhat/abaroni/jvm/tuning/empty/package-info.java
@@ -0,0 +1,1 @@
+package com.redhat.abaroni.jvm.tuning.empty;
